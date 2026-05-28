8531ba845308c8a6b6e5efcee4488d416c7f6f7b
N Manasa <manasayadavn01@gmail.com>
2026-05-28 19:48:42 +0530

    fixed cors for netlify

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors     = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000" ,   "https://resumeap.netlify.app"],
   credentials: true,
 }));
 app.use(express.json());
