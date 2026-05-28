aff122a416e84eb85f028e9463efe74fc9485de0
N Manasa <manasayadavn01@gmail.com>
2026-05-28 20:12:42 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://resume-ai-app-5n2j.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
